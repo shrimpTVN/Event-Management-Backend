82450e587bae7db68cbf704a1c6d148c634e3f05
shrimpTVN <mrshrimptran@gamil.com>
2026-09-24 23:34:11 +0700

    chore: add data for school, major, association

---
+++ ddd-infrastructure/src/main/resources/database/major-data.sql
@@ -0,0 +1,2 @@
+insert into majors(code, name, school_id, created_by)
+values ('CNTT', 'Công nghệ thông tin', 1, 1);
---
+++ ddd-infrastructure/src/main/resources/database/school-data.sql
@@ -0,0 +1,4 @@
+insert into schools(name, description, created_by)
+values ('Truờng Công nghệ thông tin và Truyền thông',
+        'Trường Công nghệ thông tin và Truyền thông là một cơ sở giáo dục chuyên đào tạo các ngành liên quan đến công nghệ thông tin, truyền thông và các lĩnh vực liên quan. Trường cung cấp các chương trình đào tạo chất lượng cao, giúp sinh viên phát triển kỹ năng và kiến thức cần thiết để đáp ứng nhu cầu của thị trường lao động hiện đại.',
+        1);
